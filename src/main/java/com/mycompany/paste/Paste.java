5ac7a4fd0a8c958692c83c90e02b45e4787bc10e
Martin Schonberger <mschonberger11@gmail.com>
2024-08-20 14:09:57 -0300

    tercer commit

---
+++ src/main/java/com/mycompany/paste/Paste.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.paste;
+
+import com.mycompany.igu.Login;
+
+public class Paste {
+
+    public static void main(String[] args) {
+        Login login = new Login();
+        login.setVisible(true);
+        login.setLocationRelativeTo(null);
+    }
+}
